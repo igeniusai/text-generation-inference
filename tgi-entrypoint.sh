53fd0651ba8808b07a2286108107b9a96f79bbb4
Paolo Albano <vincenzo.visco@igenius.ai>
2024-06-04 13:48:44 +0000

    misc: update vllm dependecy to support attention size 160

--- tgi-entrypoint.sh
+++ tgi-entrypoint.sh
@@ -2,4 +2,9 @@
 
 ldconfig 2>/dev/null || echo 'unable to refresh ld cache, not a big deal in most cases'
 
-text-generation-launcher $@
+CARGS="${CUSTOM_ARGS:=}"
+
+if [[ -z "${HF_MODEL_ID}" ]]; then
+  text-generation-launcher $CARGS $@
+else
+  text-generation-launcher $CARGS --model-id "${HF_MODEL_ID}" $@
